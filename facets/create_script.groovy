53e0a47c0e45015db4946d8b04433a044e3fb574
Luke Slater <tinmachin3@gmail.com>
2021-04-28 22:04:55 +0100

    create facet map

--- facets/create_script.groovy
+++ facets/create_script.groovy
@@ -1,3 +1,4 @@
+println "touch facet_map.txt"
 new File('./facets.txt').splitEachLine('\t') {
-  println "komenti query -q \"<${it[1]}>\" --override-group ${it[0]} --query-type subeq --class-mode --out facet_map.txt"
+  println "komenti query -q \"<${it[1]}>\" --override-group \"${it[0]}\" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP"
 }
--- facets/query_facets.sh
+++ facets/query_facets.sh
@@ -1,21 +1,22 @@
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0000818>" --override-group abnormality of the endocrine system --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0001626>" --override-group abnormality of the cardiovascular system --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0002715>" --override-group abnormality of the immune system --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0000118>" --override-group phenotypic abnormality --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0033127>" --override-group abnormality of the musculoskeletal system --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0000119>" --override-group abnormality of the genitourinary system --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0001608>" --override-group abnormality of the voice --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0001939>" --override-group abnormality of metabolism/homeostasis --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0000707>" --override-group abnormality of the nervous system --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0001507>" --override-group growth abnormality --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0025354>" --override-group abnormal cellular phenotype --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0001871>" --override-group abnormality of blood and blood\-forming tissues --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0001574>" --override-group abnormality of the integument --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0002664>" --override-group neoplasm --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0040064>" --override-group abnormality of limbs --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0045027>" --override-group abnormality of the thoracic cavity --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0025031>" --override-group abnormality of the digestive system --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0001197>" --override-group abnormality of prenatal development or birth --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0025142>" --override-group constitutional symptom --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0000152>" --override-group abnormality of head or neck --query-type subeq --class-mode --out facet_map.txt
-komenti query -q "<http://purl.obolibrary.org/obo/HP_0002086>" --override-group abnormality of the respiratory system --query-type subeq --class-mode --out facet_map.txt
+touch facet_map.txt
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0000818>" --override-group "abnormality of the endocrine system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0001626>" --override-group "abnormality of the cardiovascular system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0002715>" --override-group "abnormality of the immune system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0000118>" --override-group "phenotypic abnormality" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0033127>" --override-group "abnormality of the musculoskeletal system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0000119>" --override-group "abnormality of the genitourinary system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0001608>" --override-group "abnormality of the voice" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0001939>" --override-group "abnormality of metabolism/homeostasis" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0000707>" --override-group "abnormality of the nervous system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0001507>" --override-group "growth abnormality" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0025354>" --override-group "abnormal cellular phenotype" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0001871>" --override-group "abnormality of blood and blood\-forming tissues" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0001574>" --override-group "abnormality of the integument" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0002664>" --override-group "neoplasm" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0040064>" --override-group "abnormality of limbs" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0045027>" --override-group "abnormality of the thoracic cavity" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0025031>" --override-group "abnormality of the digestive system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0001197>" --override-group "abnormality of prenatal development or birth" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0025142>" --override-group "constitutional symptom" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0000152>" --override-group "abnormality of head or neck" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
+komenti query -q "<http://purl.obolibrary.org/obo/HP_0002086>" --override-group "abnormality of the respiratory system" --query-type subeq --class-mode --out facet_map.txt --append --ontology HP
